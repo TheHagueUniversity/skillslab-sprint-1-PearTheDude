7b7de57c5763f75e966e7245c17bfa7ccc270640
PearTheDude <a.oskarskriss@student.hhs.nl>
2017-03-23 19:58:32 +0100

    Add files via upload

---
+++ site/scripts/main.js
@@ -0,0 +1,26 @@
+var subtitle = [
+    "If i was a fish i would swim good - Martin",
+    "-placeholder-",
+    "No plebs allowed",
+    "Drink water. Or you might die",
+    "By reading this sentence you have just given me a brief control of your mind",
+    "The cake is a lie",
+    "When homeless is asking you something say CYKA BLYAT",
+    "Both bullets hit each other, so they cancle each other out, and that's how you survive",
+    "So, a dyslexic man walks into a bra...",
+    "So Jesus happenned, like 2017 years ago...",
+    "What's your heart go stronger",
+    "This thing is, like, INTENSE",
+    "Its like dying pigs you know- AWEEEEAK",
+    "You pass butter",
+    "Spice it up"
+    
+    
+    
+    
+];
+
+var randomnumber = Math.floor(Math.random() * (15 - 0 + 0)) + 0;
+console.log(randomnumber);
+
+document.getElementById('subtitle').innerHTML = subtitle[randomnumber] ;
